12fe34c226eec8ee674cb058b5c4bddc7e7b0244
Momili <32461394+Momili@users.noreply.github.com>
2018-02-23 16:19:50 -0800

    added "display" label for sorting

--- models/test_renewal.php
+++ models/test_renewal.php
@@ -1,7 +1,4 @@
-<?php 
-        
-    
-    class test_renewal{ 
+<?php class test_renewal{ 
 
  // database connection and table name 
     private $conn; 
@@ -44,12 +41,30 @@
     public $Fri;
     public $Sat;
     public $TargetTZ;
-    public $LocalTZ;
-    
+    public $LocalTZ;    
     
     // constructor with $db as database connection 
     public function __construct($db){ 
         $this->conn = $db;
+    }
+    
+     // read products
+    function readAll($userId, $shopId, $status){ 
+        // select all query
+        $query = "SELECT 
+                    ID,'false' as selected,UserID,ShopID,ItemID,Title,ImageUrl,Quantity,Views,Likes,LastUpdatedDate,ExpiryDate,ScheduledDateTime,ScheduledDate,ScheduledTime,RenewalStatus,UpdatedTimeStamp,TargetDateTime,LocalDateTime
+                  FROM " . $this->table_name . "
+                  WHERE  RenewalStatus='".$status."'
+                  AND UserID=". $userId ."
+                  AND ShopID=". $shopId ."
+                  ORDER BY 
+                  ScheduledDate,ScheduledTime";
+ 
+        // prepare query statement
+        $stmt = $this->conn->prepare( $query );     
+        // execute query
+        $stmt->execute();     
+        return $stmt;
     }
     
     function createNew(){        
@@ -100,6 +115,102 @@
         }
     }
     
+    function update_random(){
+    	try{
+    		$query ="UPDATE " .$this->table_name ."
+                    SET RenewalStatus='". $this->RenewalStatus ."',
+                    ItemID='". $this->ItemID ."',
+                    Title='". $this->Title ."',
+                    ImageUrl='". $this->ImageUrl ."',
+                    Quantity='". $this->Quantity ."',
+                    Views='". $this->Views ."',
+                    Likes='". $this->Likes ."',
+                    LastUpdatedDate='". $this->LastUpdatedDate ."',
+                    ExpiryDate='". $this->ExpiryDate ."',                    				
+                    UpdatedTimeStamp='". $this->UpdatedTimeStamp ."',
+                    RenewType='". $this->RenewType ."'
+                    WHERE ID='". $this->ID ."'" ;
+    		// prepare query
+    		$stmt = $this->conn->prepare($query);
+    		
+    		echo $query."</br>";
+    		
+    		// execute query
+    		$stmt->execute();
+    	}catch(PDOException $e){
+    		echo $query . "<br>" . $e->getMessage();
+    	}
+    }
+    
+    function update_forever(){
+    	try{
+            $query ="UPDATE " .$this->table_name ."
+                SET ScheduledDateTime='". $this->ScheduledDateTime ."',
+                LocalDateTime='". $this->LocalDateTime ."',
+                TargetDateTime='". $this->TargetDateTime ."',
+                ScheduledDate='". $this->ScheduledDate ."',
+                ScheduledTime='". $this->ScheduledTime ."',
+                UpdatedTimeStamp='". $this->UpdatedTimeStamp ."'
+                WHERE ID='". $this->ID ."'" ;
+    		// prepare query
+    		$stmt = $this->conn->prepare($query);
+    		
+    		//echo $query."</br>";
+    		
+    		// execute query
+    		$stmt->execute();
+    	}catch(PDOException $e){
+    		echo $query . "<br>" . $e->getMessage();
+    	}
+    }
+    
+    function add_forever_completed($id){
+        try{
+        $query="INSERT INTO " .$this->table_name ."(UserID,ShopID,ItemID,Title,ImageUrl,Quantity,Views,Likes,LastUpdatedDate,ExpiryDate
+                ,ScheduledDateTime,ScheduledDate,ScheduledTime,RenewalStatus,UpdatedTimeStamp,TargetDateTime,LocalDateTime,Unit,Frequency,RenewType
+                ,EndDate,EndTIme,NumberOfItems,Sun,Mon,Tue,Wed,Thu,Fri,Sat,TargetTz,LocalTz)
+                SELECT UserID,ShopID,ItemID,Title,ImageUrl,Quantity,Views,Likes,LastUpdatedDate,ExpiryDate
+                ,ScheduledDateTime,ScheduledDate,ScheduledTime,'R','". date('Y-m-d H:i:s') ."',TargetDateTime,LocalDateTime,Unit,Frequency,RenewType
+                ,EndDate,EndTIme,NumberOfItems,Sun,Mon,Tue,Wed,Thu,Fri,Sat,TargetTz,LocalTz
+                FROM " .$this->table_name ."
+                WHERE ID='". $id ."'" ;
+    		$stmt = $this->conn->prepare($query);    		
+    		$stmt->execute();
+    	}catch(PDOException $e){
+    		echo $query . "<br>" . $e->getMessage();
+    	}
+    }
+    
+     function add_scheduled_item($id, $Item_id, $title, $image_url, $quantity, $views, $likes, $last_updated_date, $expiry_date ){
+        try{
+        $query="INSERT INTO " .$this->table_name ."(UserID,ShopID,ItemID,Title,ImageUrl,Quantity,Views,Likes,LastUpdatedDate,ExpiryDate
+                ,ScheduledDateTime,ScheduledDate,ScheduledTime,RenewalStatus,UpdatedTimeStamp,TargetDateTime,LocalDateTime,Unit,Frequency,RenewType)
+                SELECT UserID,ShopID,'". $Item_id."','". $title."','". $image_url."','". $quantity."','". $views."','". $likes."','". $last_updated_date->format('Y-m-d H:i:s')."','". $expiry_date->format('Y-m-d H:i:s')."'
+                ,ScheduledDateTime,ScheduledDate,ScheduledTime,'R','". date('Y-m-d H:i:s') ."',TargetDateTime,LocalDateTime,Unit,Frequency,RenewType
+                FROM " .$this->table_name ."
+                WHERE ID='". $id ."'" ;
+    		$stmt = $this->conn->prepare($query);
+    		$stmt->execute();
+    	}catch(PDOException $e){
+    		echo $query . "<br>" . $e->getMessage();
+    	}
+    }
+    
+    function update($status, $id){//should be update_status
+        try{            
+            $query ="UPDATE " .$this->table_name ."
+                    SET RenewalStatus='". $status ."',
+                    UpdatedTimeStamp='". date('Y-m-d H:i:s') ."' 
+                    WHERE ID='". $id ."'" ;
+            // prepare query
+            $stmt = $this->conn->prepare($query); 
+            // execute query
+            $stmt->execute(); 
+        }catch(PDOException $e){
+            echo $query . "<br>" . $e->getMessage();
+        }
+    }
+    
     function get_scheduled_items(){
         // select all query
         $query = "SELECT 
@@ -113,6 +224,13 @@
                   AND r.ScheduledDateTime<=Now() 
                   AND r.ScheduledDateTime>=DATE_SUB(Now(),INTERVAL 2 hour)
                   AND u.expiry_date>=Now()
+                  AND ((DAYOFWEEK(CURRENT_DATE())=1 AND r.Sun='Y') OR
+                       (DAYOFWEEK(CURRENT_DATE())=2 AND r.Mon='Y') OR
+                       (DAYOFWEEK(CURRENT_DATE())=3 AND r.Tue='Y') OR
+                       (DAYOFWEEK(CURRENT_DATE())=4 AND r.Wed='Y') OR
+                       (DAYOFWEEK(CURRENT_DATE())=5 AND r.Thu='Y') OR
+                       (DAYOFWEEK(CURRENT_DATE())=6 AND r.Fri='Y') OR
+                       (DAYOFWEEK(CURRENT_DATE())=7 AND r.Sat='Y'))
                   ORDER BY ScheduledDateTime";
                           
         echo($query);
@@ -126,5 +244,88 @@
         // execute query
         $stmt->execute();     
         return $stmt;
+    }
+    
+    function read_all_renewals($month){
+        // select all query
+        $query = "SELECT r.ID, r.ImageUrl, r.Title, r.ScheduledDateTime, r.UpdatedTimeStamp, r.TargetDateTime, r.LocalDateTime, r.RenewalStatus, e.shop_name, r.RenewType, e.login_name
+                  FROM " . $this->table_name . " r 
+                  inner join etsy_users  e on r.ShopID = e.shop_id and r.UserID = e.google_id
+                  WHERE  Month(ScheduledDateTime)=".$month."
+                  ORDER BY r.RenewalStatus DESC, r.ScheduledDateTime";
+        
+        //echo($query);
+        
+        // prepare query statement
+        $stmt = $this->conn->prepare( $query );     
+        // execute query
+        $stmt->execute();     
+        return $stmt;
+    }
+    
+    function delete_all_scheduled($user_id, $shop_id){
+    	       
+        try{            
+            $query ="UPDATE " .$this->table_name ."
+                    SET RenewalStatus='C', 
+                    UpdatedTimeStamp='". date('Y-m-d H:i:s') ."' 
+                    WHERE UserID=". $user_id ." 
+                    AND ShopID=". $shop_id ."
+                    AND (RenewalStatus='S' OR RenewalStatus='F') ";
+            
+            echo $query;
+            // prepare query
+            $stmt = $this->conn->prepare($query); 
+            // execute query
+            $stmt->execute(); 
+        }catch(PDOException $e){
+            echo $query . "<br>" . $e->getMessage();
+        }
+    }    
+	
+    function read_completed($userId, $shopId, $month){
+    	// select all query
+    	$query = "SELECT
+                    ID,'false' as selected,UserID,ShopID,ItemID,Title,ImageUrl,Quantity,Views,Likes,LastUpdatedDate,ExpiryDate,ScheduledDateTime,ScheduledDate,ScheduledTime,RenewalStatus,UpdatedTimeStamp,TargetDateTime,LocalDateTime,RenewType
+                  FROM " . $this->table_name . "
+                  WHERE  RenewalStatus='R'
+                  AND UserID=". $userId ."
+                  AND ShopID=". $shopId ."
+                  AND Month(ScheduledDateTime)=".$month."
+                  ORDER BY ScheduledDateTime DESC";
+    
+    	// prepare query statement
+    	$stmt = $this->conn->prepare( $query );
+    	// execute query
+    	$stmt->execute();
+    	return $stmt;
+    }
+    
+    function read_scheduled($userId, $shopId){ 
+        // select all query
+        $query = "SELECT 
+                    ID,'false' as selected,UserID,ShopID,ItemID,Title,ImageUrl,Quantity,Views,Likes,LastUpdatedDate,ExpiryDate,ScheduledDateTime,ScheduledDate,ScheduledTime,RenewalStatus,UpdatedTimeStamp,TargetDateTime,LocalDateTime,Unit,Frequency
+                  FROM " . $this->table_name . "
+                  WHERE  (RenewalStatus='S' OR RenewalStatus='F')
+                  AND UserID=". $userId ."
+                  AND ShopID=". $shopId ."
+                  ORDER BY ScheduledDateTime";                  
+ 
+        // prepare query statement
+        $stmt = $this->conn->prepare( $query );     
+        // execute query
+        $stmt->execute();     
+        return $stmt;
+    }
+    
+    function delete_all_renewals(){
+        
+        $query ="Delete from " .$this->table_name;
+            
+        echo $query;
+        // prepare query
+        $stmt = $this->conn->prepare($query); 
+        // execute query
+        $stmt->execute();        
     }
 }
